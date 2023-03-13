ea523c0cb6e369567245144dcd76ac5a9feae650
RkVirus <ravim70655@gmail.com>
2023-03-13 19:21:24 +0530

    music player is ready

--- src/Components/MusicCard.tsx
+++ src/Components/MusicCard.tsx
@@ -1,36 +1,89 @@
-import React from 'react';
-import { Card, CardBody,CardTitle,CardSubtitle,CardText,Button } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, CardBody, CardTitle, CardSubtitle, Button, Progress } from 'reactstrap';
+import { Pause, SkipForward, SkipBackward, FastForward, Play } from 'react-bootstrap-icons';
+import '../App.css';
 
 function MusicCard() {
+  const [audio, setAudio] = useState(new Audio('/waterfall.mp3'))
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [pointer, setPointer] = useState(0)
+  const [currTime, setCurrTime] = useState(0)
+  const [duration, setDuration] = useState(audio.duration)
+
+ // changing progress
+  audio.addEventListener('timeupdate', ()=>{
+    console.log(audio.currentTime)
+    let value = (audio.currentTime / audio.duration)*100
+    setPointer(value)
+    setCurrTime(Math.round(audio.currentTime))
+  })
+
+  const playOrPause = (isPlaying: any) =>{
+    setIsPlaying(!isPlaying)
+    if(isPlaying){
+      audio.pause()
+    }
+    else{
+      audio.play();
+    }
+  }
   return (
     <>
-    <Card
-  style={{
-    width: '18rem',
-    padding:30
-  }}
->
-  <img
-    alt="card image"
-    src="https://picsum.photos/200/200"
-    style={{borderRadius:'50%'}}
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Music Name
-    </CardTitle>
-    <CardSubtitle
-      className="mb-4 text-muted"
-      tag="h6"
-    >
-      Music by RkVirus
-    </CardSubtitle>
-    <Button>
-      Play/Pause
-    </Button>
-  </CardBody>
-</Card>
-</>
+      <Card
+        style={{
+          width: '20rem',
+          padding: 30
+        }}
+      >
+        <img
+          alt="card image"
+          src="https://picsum.photos/200/200"
+          style={{
+            borderRadius: '50%',
+          }}
+        // className='App-logo'
+        />
+        <CardBody>
+          <CardTitle tag="h5">
+            Music Name
+          </CardTitle>
+          <CardSubtitle
+            className="mb-4 text-muted"
+            tag="h6"
+          >
+            Music by RkVirus
+          </CardSubtitle>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div className="text-start text-black" style={{ fontSize: '0.8rem' }}>
+              {currTime}sec
+            </div>
+            <div className="text-start text-black" style={{ fontSize: '0.8rem' }}>
+              {duration}
+            </div>
+          </div>
+          <Progress
+            max="100"
+            value={pointer}
+          />
+          <audio src="./waterfall.mp3"></audio>
+          <div className='d-flex justify-content-between px-4'>
+            <Button className='mt-4'>
+              <SkipBackward />
+            </Button>
+            <Button className='mt-4'
+              onClick={()=>{
+                playOrPause(isPlaying)
+              }}
+            >
+              {isPlaying?<Pause/>: <Play/>}
+            </Button>
+            <Button className='mt-4'>
+              <SkipForward />
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
+    </>
   )
 }
 
